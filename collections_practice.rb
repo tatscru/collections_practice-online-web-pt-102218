32acfeffb8003abbb791ea04740560399f4e5f26
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 13:27:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(integers)
+  integers.sort  
+end 
 
+def sort_array_desc(integers)
+  integers.sort do |a,b| 
+    b <=> a
+  end 
+end 
+
+def sort_array_char_count(integers)
+  integers.sort do |a,b|
+    a.length <=> b.length
+  end 
+end 
+
+def swap_elements(integers)
+  
+
+end 
+
+def reverse_array(integers)
+  integers.reverse
+end 
+
+def kesha_maker(integers)
+  integers.each do |kesha|
+    kesha[2] = "$"
+  end 
+end 
+
+def find_a(integers)
+  integers.select do |letter|
+    letter[0] == "a"
+  end 
+end
+
+def sum_array
+
+end 
+
+
+def add_s
+  
+end 
